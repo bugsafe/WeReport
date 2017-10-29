f80b8657e0c25f17da652ca3faeaa9eda6bc946a
bugsafe <177705712@qq.com>
2017-10-29 17:58:14 +0800

    WeReport V1.0

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,9 +2,9 @@
 return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
 	'DB_HOST'   => 'localhost', // 服务器地址
-	'DB_NAME'   => 'a0727145502', // 数据库名
-	'DB_USER'   => 'a0727145502', // 用户名
-	'DB_PWD'    => '3023925', // 密码
+	'DB_NAME'   => 'wereport', // 数据库名
+	'DB_USER'   => 'root', // 用户名
+	'DB_PWD'    => 'root', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'we_', // 数据库表前缀 
 	'DB_CHARSET'=> 'utf8', // 字符集
